c75e4a83b8d4a6d05bd138cafdd6efcf6532ce5b
MostafaMohamed122 <167568972+MostafaMohamed122@users.noreply.github.com>
2024-12-23 07:50:52 +0200

    Create db.php

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$dbname = 'fcds';
+$username = 'root';
+$password = 'Ahmedali123';
+
+// Create a PDO instance
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
